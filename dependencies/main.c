1a0e741d9f04345a56b7647a70d0fbd6c7e0e50c
Pablo Ridolfi <pridolfi@frh.utn.edu.ar>
2015-10-02 18:06:53 -0300

    dependencies example

---
+++ dependencies/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+   printf("suma: %d\n", NUM1 + NUM2);
+   return 0;
+}
---
+++ dependencies/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define NUM1 3
+#define NUM2 1
+
+#endif /* MAIN_H */
